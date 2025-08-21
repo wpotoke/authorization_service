a5abadb774f336d21a2d5b89f76ea0bd828580a0
moroz <morozovdaniel713@gmail.com>
2025-08-21 16:42:46 +0300

    delete(database.py): Delete database.py     rename database.py in engine.py
    feat(engine.py) Implement async database engine and session management:
    - Create async session with connection pooling and bind engine to it
    - Configure async session maker get_db with error handling, rollback changes
    and session closing
    - Definite function for manage db:
        - create_db() for init db creation
        - drop_db() for clean db(testing purposes!)
        - close_engine() for correct disconn
    - Use environment variable DB_URL for database connection string

---
+++ engine.py
@@ -0,0 +1,47 @@
+import os
+
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.exc import SQLAlchemyError
+
+from models import Base
+
+
+engine = create_async_engine(os.getenv("DB_URL"), echo=True, pool_pre_ping=True)
+
+session_maker = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+    autocommit=False,
+)
+
+
+@asynccontextmanager
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    session: AsyncSession = session_maker()
+    try:
+        yield session
+        await session.commit()
+    except SQLAlchemyError as e:
+        await session.rollback()
+        raise SQLAlchemyError from e
+    finally:
+        await session.close()
+
+
+async def create_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def drop_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+
+async def close_engine():
+    await engine.dispose()
