3ad4ae3bfddac73588bd4e4b8a5cf2c4c8c71e54
Danila Morozov <146642624+wpotoke@users.noreply.github.com>
2025-08-22 08:10:06 +0300

    feat(TokenData): Add TokenData for prevents processing of incorrect data - token has expected fields(validate) - clean code separate token for token data
    Create README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+min work version
--- schemas.py
+++ schemas.py
@@ -26,3 +26,7 @@ class UserResponse(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class TokenData(BaseModel):
+    email: str
